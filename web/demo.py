68dc7195f593bc8976d885c41c9cdc3e0876b311
chen <1773104733@qq.com>
2024-04-19 17:32:14 +0800

    log

--- web/demo.py
+++ web/demo.py
@@ -12,6 +12,27 @@ class hello:
             name = 'World'
         return 'Hello,' + name
     
+    
+class MiddlewareExample(object):
+    def __init__(self, app):
+        self._app = app
+        
+    def __call__(self, environ, start_response):
+        if not self._app:
+            return ['Advanced return !']
+        # 调用下一层
+        return self._app(environ, start_response)
+    
+def create_example_app(app):
+    return MiddlewareExample(
+        MiddlewareExample(
+            MiddlewareExample(
+                MiddlewareExample(app)
+            )
+        )
+    )
+    
 if __name__ == "__main__":
+    # app.wsgifunc()
     app.run()
 
